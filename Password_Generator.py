5218e95595242cf86641bc23da3b7f86e073029e
Kenny Ng <J356934@tafe.wa.edu.au>
2022-08-04 11:13:03 +0800

    version3

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,8 +1,14 @@
+# Edited password length
+
 import random
 
 password = ''
-
+while True:
+    password_length = int(input("Please enter length of password: (1-49)"))
+    if 0 < password_length <50:
+        break
 for i in range(10):
-    random_integer = random.randint(0,126)
+    random_integer = random.randint(33,126)
     password += chr(random_integer)
 print(password, len(password))
+
