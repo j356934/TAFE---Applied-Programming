9396736e87222ee2e63d18443b4a71ad970a7a62
Kenny Ng <J356934@tafe.wa.edu.au>
2022-08-04 11:26:01 +0800

    added comment

--- Password_Generator.py
+++ Password_Generator.py
@@ -10,5 +10,5 @@ while True:
 for i in range(10):
     random_integer = random.randint(33,126)
     password += chr(random_integer)
-print(password, len(password))
+print("Password ", password, len(password))
 
